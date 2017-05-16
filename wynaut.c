cf82a479e6b902f12ed4b0ff6328b24f3fbf0325
JaiGatsby <jais334@gmail.com>
2017-05-16 22:52:12 +0800

    Finished backspace function

--- wynaut.c
+++ wynaut.c
@@ -264,6 +264,21 @@ void editorAppendRow(char* s, size_t len){
 	E.dirty++;
 }
 
+// frees from memory a given row and the chars in it
+void editorFreeRow(erow* row){
+	free(row->render);
+	free(row->chars);
+}
+
+// handles deleting a char if it happens to be the start of the row
+void editorDelRow(int at){
+	if (at < 0 || at >= E.numrows) return;
+	editorFreeRow(&E.row[at]);
+	memmove(&E.row[at],&E.row[at+1],sizeof(erow)*(E.numrows-at-1));
+	E.numrows--;
+	E.dirty++;
+}
+
 // deletes characters given position
 void editorRowDelChar(erow* row, int at){
 	if(at<0 || at >= row->size) return;
@@ -290,6 +305,15 @@ void editorRowInsertChar(erow* row, int at, int c){
 	E.dirty++;
 }
 
+void editorRowAppendString(erow* row, char* s, size_t len){
+	row->chars = realloc(row->chars,row->size +len+1);
+	memcpy(&row->chars[row->size],s,len);
+	row->size += len;
+	row->chars[row->size] = '\0';
+	editorUpdateRow(row);
+	E.dirty++;
+}
+
 /** editor operations **/
 
 // Deals with where the cursor is and adds a char
@@ -304,10 +328,17 @@ void editorInsertChar(int c){
 // Mapped to Backspace character
 void editorDelChar(){
 	if(E.cy == E.numrows) return;
+	if(E.cx==0 && E.cy == 0)return;
 	erow* row = &E.row[E.cy];
 	if (E.cx >0){
 		editorRowDelChar(row,E.cx-1);
 		E.cx--;
+	}
+	else{ // moves everything to the end of the last line and deletes current row
+		E.cx = E.row[E.cy-1].size;
+		editorRowAppendString(&E.row[E.cy-1],row->chars,row->size);
+		editorDelRow(E.cy);
+		E.cy--;
 	}
 }
 
