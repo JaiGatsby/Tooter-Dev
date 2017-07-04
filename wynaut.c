1cc801f85128633dbad9a58e62f3ebcd2b8084c0
JaiGatsby <jais334@gmail.com>
2017-07-04 18:40:15 +0800

    Introduced syntax highlighting structure

--- wynaut.c
+++ wynaut.c
@@ -1,4 +1,4 @@
-// @TODO Step 142
+// @TODO Step 150
 // consider adding fuzzy search 
 /** includes **/
 
@@ -44,6 +44,11 @@ enum editorKey{
 	PAGE_DOWN
 };
 
+enum editorHighlight {
+	HL_NORMAL = 0,
+	HL_NUMBER
+};
+
 /** data **/
 
 // defines datatype e(each)row to be a struct with char array and size
@@ -52,7 +57,7 @@ typedef struct erow {
 	int rsize;
 	char* chars;
 	char* render;
-
+	unsigned char* hl;
 } erow;
 
 struct editorConfig {
@@ -214,6 +219,31 @@ int getWindowsSize(int* rows, int* cols){
 	}
 }
 
+/** syntax highlighting **/
+
+void editorUpdateSyntax(erow* row){
+	row->hl = realloc(row->hl, row->rsize);
+	//set all characters to HL_NORMAL by default
+	memset(row->hl, HL_NORMAL, row->rsize);
+	
+	// now we loop over the digits and change the digits to HL_NUMBER
+	int i;
+	for (i=0; i<row->rsize; i++){
+		if (isdigit(row->render[i])){
+			row->hl[i] = HL_NUMBER;
+		}
+	}
+}
+
+int editorSyntaxToColor(int hl){
+	switch(hl){
+		case HL_NUMBER:
+			return 31;
+		default:
+			return 37;
+	}
+}
+
 /** row operations **/
 
 int editorRowCxToRx(erow* row, int cx){
@@ -261,6 +291,8 @@ void editorUpdateRow(erow* row){
 	}
 	row->render[idx] = '\0';
 	row->rsize = idx;
+
+	editorUpdateSyntax(row);
 }
 
 void editorInsertRow(int at,char* s, size_t len){
@@ -277,6 +309,7 @@ void editorInsertRow(int at,char* s, size_t len){
 
 	E.row[at].rsize = 0;
 	E.row[at].render = NULL;
+	E.row[at].hl = NULL;
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
@@ -287,6 +320,7 @@ void editorInsertRow(int at,char* s, size_t len){
 void editorFreeRow(erow* row){
 	free(row->render);
 	free(row->chars);
+	free(row->hl);
 }
 
 // handles deleting a char if it happens to be the start of the row
@@ -595,7 +629,30 @@ void editorDrawRows(struct abuf* ab){
 			int len = E.row[filerow].rsize - E.coloff;
 			if (len <0) len =0;
 			if (len > E.screencols) len = E.screencols;
-			abAppend(ab, &E.row[filerow].render[E.coloff], len);
+			char* c = &E.row[filerow].render[E.coloff];
+			unsigned char* hl = &E.row[filerow].hl[E.coloff];
+			int current_color = -1;
+			int j;
+			// Prints char by char, if its a digit, then changes color and adds it
+			for (j=0; j<len; j++){
+				if (hl[j] == HL_NORMAL){
+					if (current_color != -1){
+						abAppend(ab, "\x1b[39;1m",7); // code reset colors
+						current_color = -1;
+					}
+					abAppend(ab, &c[j], 1);
+				} else {
+					int color = editorSyntaxToColor(hl[j]);
+					if (color != current_color){
+						current_color = color;
+						char buf[16];
+						int clen = snprintf(buf, sizeof(buf), "\x1b[%d;1m",color);
+						abAppend(ab, buf, clen);
+					}
+					abAppend(ab, &c[j], 1);
+				}
+			}
+			abAppend(ab, "\x1b[39;1m",7);
 		}
 		// 4 -> writing 4 bytes to terminal
 		// \x1b is the escape character
